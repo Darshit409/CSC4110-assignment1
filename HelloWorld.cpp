3029ffba99863995af3efcf50601985cf129fda1
darshit tailor <gr9446@wayne.edu>
2019-09-14 12:49:11 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,7 @@
 // Simple C++ program to display "Hello World" 
+//Auther: Darshit Tailor<gr9446@wayne.edu>
 // Header file for input output functions 
+
 #include<iostream>  
   
 using namespace std; 
